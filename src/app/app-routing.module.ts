f91747966fc94576db900bd8262a8dc67391260b
Utharaanil <utharav15@gmail.com>
2023-08-14 11:56:50 +0530

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LeadsComponent } from './leads/leads.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'',redirectTo:'dashboard',pathMatch:'full'},
+  {path:'employee',component:EmployeeComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'contact',component:ContactsComponent},
+  {path:'leads',component:LeadsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
